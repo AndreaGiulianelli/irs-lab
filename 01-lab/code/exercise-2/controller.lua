18fdd3cc3e57672f4ea084af5148142afd6ec878
Andrea Giulianelli <andrygiulio@gmail.com>
2023-03-04 19:37:33 +0100

    feat: add second solution to exercise 2

--- 01-lab/code/exercise-2/controller.lua
+++ 01-lab/code/exercise-2/controller.lua
@@ -1,7 +1,9 @@
 MAX_VELOCITY = 10
 MOVE_STEPS = 15
+MAX_PROXIMITY_THRESHOLD = 0.15
 
 n_steps = 0
+avoiding = false -- Variable that indicate whether the robot is currently avoiding an obstacle
 
 function init()
     -- In the init start to move randomly
@@ -15,28 +17,48 @@ function step()
     max_proximity_value = -1
     max_proximity_index = -1
     sum = 0
+    proximity_sensor_to_use = { 1,2,3,24,23,22}
     -- Get the maximum value and index only for logging purposes.
-    for i=1,#robot.proximity do
-        if max_proximity_value < robot.proximity[i].value then
-            max_proximity_index = i
-            max_proximity_value = robot.proximity[i].value
+    for i=1,#proximity_sensor_to_use do
+        index = proximity_sensor_to_use[i]
+        if max_proximity_value < robot.proximity[index].value then
+            max_proximity_index = index
+            max_proximity_value = robot.proximity[index].value
         end
-        sum = sum + robot.proximity[i].value
+        sum = sum + robot.proximity[index].value
     end
 
+    -- Logging
     log("Max proximity " .. max_proximity_value .. " - index: " .. max_proximity_index)
+    if max_proximity_value == 1 then
+        log("PROBABLE COLLISION")
+    end
     
-    if (sum == 0) then
-        -- If the robot is stuck or is completely free then move randomly
+    if (sum == 0) or (max_proximity_value < MAX_PROXIMITY_THRESHOLD) then
+        -- If the robot is stuck or is completely free or is 
+        -- not enough near to an obstacle then move randomly
         if n_steps % MOVE_STEPS == 0 then
             left_v = robot.random.uniform(0,MAX_VELOCITY)
             right_v = robot.random.uniform(0,MAX_VELOCITY)
-            n_steps = 0 -- To avoid overflow.
+            n_steps = 0
         end
+        avoiding = false -- Reset variable
     else
-        -- There is an obstacle in the surrounding, turn left.
-        left_v = robot.random.uniform(0,MAX_VELOCITY)
-        right_v = 0
+        -- There is an obstacle in the surrounding.
+        -- Check if it is already turning to avoid it or not.
+        if avoiding == false then
+            -- First time that the robot sees that obstacle, turn.
+            -- Random direction to turn: true right, false left
+            avoiding = true
+            if robot.random.bernoulli() then 
+                left_v = MAX_VELOCITY
+                right_v = -MAX_VELOCITY
+            else
+                left_v = -MAX_VELOCITY
+                right_v = MAX_VELOCITY
+            end
+        end
+        -- else continue to turn in the direction decided before
     end
     
     robot.wheels.set_velocity(left_v,right_v) 
---
+++ 01-lab/code/exercise-2/controller0.lua
@@ -0,0 +1,51 @@
+MAX_VELOCITY = 10
+MOVE_STEPS = 15
+
+n_steps = 0
+
+function init()
+    -- In the init start to move randomly
+    left_v = robot.random.uniform(0,MAX_VELOCITY)
+    right_v = robot.random.uniform(0,MAX_VELOCITY)
+    robot.wheels.set_velocity(left_v,right_v)
+end
+
+function step()
+    n_steps = n_steps + 1
+    max_proximity_value = -1
+    max_proximity_index = -1
+    sum = 0
+    -- Get the maximum value and index only for logging purposes.
+    for i=1,#robot.proximity do
+        if max_proximity_value < robot.proximity[i].value then
+            max_proximity_index = i
+            max_proximity_value = robot.proximity[i].value
+        end
+        sum = sum + robot.proximity[i].value
+    end
+
+    log("Max proximity " .. max_proximity_value .. " - index: " .. max_proximity_index)
+    
+    if (sum == 0) then
+        -- If the robot is stuck or is completely free then move randomly
+        if n_steps % MOVE_STEPS == 0 then
+            left_v = robot.random.uniform(0,MAX_VELOCITY)
+            right_v = robot.random.uniform(0,MAX_VELOCITY)
+            n_steps = 0 -- To avoid overflow.
+        end
+    else
+        -- There is an obstacle in the surrounding, turn left.
+        left_v = 0
+        right_v = MAX_VELOCITY
+    end
+    
+    robot.wheels.set_velocity(left_v,right_v) 
+end
+
+function reset()
+
+end
+
+function destroy()
+
+end
